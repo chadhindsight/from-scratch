045df743b5ab42c411d9f882901f96ebff105eb4
Chad Hinds <chadrickj8@gmail.com>
2020-06-26 09:46:41 -0400

    Initial setup of counter functionality

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {hot} from 'react-hot-loader';
+
+class App extends React.Component {
+    state = {counter: 0}
+
+    render() {
+        return (
+          <div>
+            <p>counter: {this.state.counter}</p>
+            <button
+              onClick={() =>
+                this.setState((prev) => ({ counter: prev.counter + 1 }))
+              }
+            >+</button>
+          </div>
+        );
+    }
+}
+
+export default hot(module)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App  from './App'
 
-const App = () => {
-    return (
-        <div>
-           hello world 
-        </div>
-    );
-};
 //This links your entry component to your public html
 ReactDOM.render(<App />, document.getElementById("app"));
 export default App;
